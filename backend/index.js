5880f03878d519863bf907a35fa708316a9b1d85
daksh3003 <dakshpandey3003@gmail.com>
2024-05-27 16:14:52 +0530

    Routes and authentication endpoints initialised

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,17 @@
 const connectToMongo = require('./db');
 connectToMongo();
 
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('hello world')
-})
+//middleware to deal with request's body
+app.use(express.json());
+
+
+//available routes
+app.use('/api/auth',require('./routes/auth'));
+app.use('/api/notes',require('./routes/notes'));
 
 app.listen(port,()=>{
     console.log(`app is listening at http://localhost:${port}`)
