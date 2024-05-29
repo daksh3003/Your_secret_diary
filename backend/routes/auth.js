6d40f2c8899dd64b3cc2608aea028cf1c476f305
daksh3003 <dakshpandey3003@gmail.com>
2024-05-29 17:51:41 +0530

    login authentication and user data display performed

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,11 +3,13 @@ const User = require("../models/User");
 const router = express.Router();
 const { body, validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
-const jwt = require('jsonwebtoken');
-//create an user using POST "/api/auth/"
+const jwt = require("jsonwebtoken");
+const { ValidatorsImpl } = require("express-validator/lib/chain");
+const fetchuser = require("../middleware/fetchuser");
 
-const JWT_SECRET = "unagiisasalmonskinroll";//any string will work
+const JWT_SECRET = "unagiisasalmonskinroll"; //any string will work
 
+//Route1: create an user using POST "/api/auth/"
 router.post(
   "/createuser",
   [
@@ -32,24 +34,77 @@ router.post(
           .json({ error: "Sorry the email is already in use" });
       }
       const salt = await bcrypt.genSalt(10);
-      const secPass = await bcrypt.hash(req.body.password,salt);
+      const secPass = await bcrypt.hash(req.body.password, salt);
       user = await User.create({
         name: req.body.name,
         email: req.body.email,
         password: secPass,
       });
       const data = {
-        user:{
-          id:user.id,
-        }
-      }
-      const authToken = jwt.sign(data,JWT_SECRET);
-      res.json({authToken});
+        user: {
+          id: user.id,
+        },
+      };
+      const authToken = jwt.sign(data, JWT_SECRET);
+      res.json({ authToken });
     } catch (error) {
       console.log(error.message);
-      res.status(500).send("some error occured");
+      res.status(500).send("Internal Server Error");
+    }
+  }
+);
+//Route2: end point to authenticate an user: POST "/api/auth/login"
+router.post(
+  "/login",
+  [
+    body("email", "Enter a valid Email").isEmail(),
+    body("password", "password cannot be empty").exists(),
+  ],
+  async (req, res) => {
+    //handling errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res
+          .status(400)
+          .json({ error: "try to login with correct credentials" });
+      }
+      const passwordCompare = await bcrypt.compare(password, user.password);
+      if (!passwordCompare) {
+        return res
+          .status(400)
+          .json({ error: "try to login with correct credentials" });
+      }
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+      const authToken = jwt.sign(payload, JWT_SECRET);
+      res.json({ authToken });
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).send("Internal Server Error");
     }
   }
 );
 
+//Route3: endpoint to get the user details: POST "api/auth/getuser" login required
+router.post("/getuser",fetchuser,
+  async (req, res) => {
+    try {
+      const userId = req.user.id;
+      const user = await User.findById(userId).select("-password");
+      res.send(user);
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).send("Internal Server Error");
+    }
+  }
+);
 module.exports = router;
