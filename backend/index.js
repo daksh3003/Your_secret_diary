b8260faa6fc180af3feb6bbf2b615411c267c784
daksh3003 <dakshpandey3003@gmail.com>
2024-06-03 20:26:57 +0530

    minor production change

--- backend/index.js
+++ backend/index.js
@@ -14,5 +14,5 @@ app.use('/api/auth',require('./routes/auth'));
 app.use('/api/notes',require('./routes/notes'));
 
 app.listen(port,()=>{
-    console.log(`app is listening at http://localhost:${port}`)
+    console.log(`your secret diary is listening at http://localhost:${port}`)
 })
