b2270483bcd0c9d0a86a66727ff719c4f9444737
daksh3003 <dakshpandey3003@gmail.com>
2024-06-07 23:23:39 +0530

    bootstrap modal created

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,22 +1,75 @@
-import React,{useContext, useEffect} from "react";
+import React, { useContext, useEffect, useRef } from "react";
 import NoteContext from "../context/notes/noteContext";
 import NoteItem from "./NoteItem";
 import AddNote from "./AddNote";
 const Notes = () => {
   const context = useContext(NoteContext);
-  const { notes,getNotes} = context;
-  useEffect(()=>{
-    getNotes()
-  },[])
+  const { notes, getNotes } = context;
+  useEffect(() => {
+    getNotes();
+    //eslint-disable-next-line
+  }, []);
+  const updateNote = (note) => {
+    ref.current.click();
+  };
+  const ref = useRef(null);
   return (
     <>
-    <AddNote/>
-    <div className="row my-3">
-      <h2>Your notes!</h2>
-      {notes.map((note) => {
-        return <NoteItem key={note._id} note={note}/>
-      })}
-    </div>
+      <AddNote />
+      <button
+        type="button"
+        className="btn btn-primary"
+        data-bs-toggle="modal"
+        data-bs-target="#exampleModal"
+        ref={ref}
+      >
+        Launch demo modal
+      </button>
+
+      <div
+        className="modal fade"
+        id="exampleModal"
+        tabindex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h1 className="modal-title fs-5" id="exampleModalLabel">
+                Modal title
+              </h1>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="modal-body">...</div>
+            <div className="modal-footer">
+              <button
+                type="button"
+                className="btn btn-secondary"
+                data-bs-dismiss="modal"
+              >
+                Close
+              </button>
+              <button type="button" className="btn btn-primary">
+                Save changes
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="row my-3">
+        <h2>Your notes!</h2>
+        {notes.map((note) => {
+          return (
+            <NoteItem key={note._id} updateNote={updateNote} note={note} />
+          );
+        })}
+      </div>
     </>
   );
 };
--- src/context/notes/noteState.js
+++ src/context/notes/noteState.js
@@ -46,7 +46,17 @@ const NoteState = (props) => {
     console.log("adding a new note");
   };
   //Delete a note
-  const deleteNote = (id) => {
+  const deleteNote = async(id) => {
+    //api call
+    const response = await fetch(`${host}/api/notes/deletenotes/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        "authToken":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjY1NzFjOWIzZTFiZjEzNjgzZDQ2NWE1In0sImlhdCI6MTcxNjk4NTE1N30.Q_BhbCOhBrx0UUsIb1HUupKBEcDr14mbyhsFVqrpMR0"
+      },
+    });
+    const json =  await response.json();
+    console.log(json)
     console.log("delete the note with id: " + id);
     const newNotes = notes.filter((note) => {
       return note._id !== id;
